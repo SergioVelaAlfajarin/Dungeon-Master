f414cbf3fa9bd249bbb81c63591b9f33bf36e642
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2023-04-26 09:04:21 +0200

    Creadas actividades basicas

---
+++ app/src/main/java/sva/dungmas/RestZoneActivity.kt
@@ -0,0 +1,11 @@
+package sva.dungmas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestZoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_zone)
+    }
+}
