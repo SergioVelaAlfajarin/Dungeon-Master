8349d3bdcff534396c3369fd3d1e0966bdae179b
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2023-04-28 14:14:06 +0200

    ajustado interfaces
    Ajustada interfaz ajustes.
    agregado funcionalidad a botones de MainActivity.

--- app/src/main/java/sva/dungmas/CreatePlayerActivity.kt
+++ app/src/main/java/sva/dungmas/CreatePlayerActivity.kt
@@ -7,5 +7,7 @@ class CreatePlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_player)
+
+
     }
 }
--- app/src/main/java/sva/dungmas/MainActivity.kt
+++ app/src/main/java/sva/dungmas/MainActivity.kt
@@ -1,11 +1,40 @@
 package sva.dungmas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setButtonsEvents()
+    }
+
+    private fun setButtonsEvents(){
+        (findViewById<Button>(R.id.btnStartGame))
+           .setOnClickListener(this::btnStartGameClick)
+        (findViewById<Button>(R.id.btnRanking))
+            .setOnClickListener(this::btnRankingClick)
+        (findViewById<Button>(R.id.btnSettings))
+            .setOnClickListener(this::btnSettingsClick)
+    }
+
+    private fun btnStartGameClick(v: View){
+        val it = Intent(this, CreatePlayerActivity::class.java)
+        startActivity(it)
+    }
+
+    private fun btnRankingClick(v: View){
+        val it = Intent(this, RankingActivity::class.java)
+        startActivity(it)
+    }
+
+    private fun btnSettingsClick(v: View){
+        val it = Intent(this, SettingsActivity::class.java)
+        startActivity(it)
     }
 }
