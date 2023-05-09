df2d69a95f0fb792ca2a3af102b4d2bbff5c7936
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2023-05-09 23:32:04 +0200

    continuado, no terminado

--- app/src/main/java/sva/dungmas/dialogs/ConfirmDialog.kt
+++ app/src/main/java/sva/dungmas/dialogs/ConfirmDialog.kt
@@ -5,8 +5,6 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
-import sva.dungmas.R
-import sva.dungmas.game.Game
 
 class ConfirmDialog(
     private val title: String,
